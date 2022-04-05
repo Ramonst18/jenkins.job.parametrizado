d2ff0374313336acd22f67ee6fa2fdf91a06f2c5
Ramón López <63748740+Ramonst18@users.noreply.github.com>
2022-04-04 18:48:06 -0700

    Update paramtrizadoDSL.groovy

--- paramtrizadoDSL.groovy
+++ paramtrizadoDSL.groovy
@@ -1,39 +1,40 @@
 job('ejemplo2-job-DSL'){
-	description('Job DSL de ejemplo para el curso de Jenkins')
-  	scm {
-      git('https://github.com/macloujulian/jenkins.job.parametrizado.git', 'main') { node ->
-      	node / gitConfigName('Ramon Lopez')
-      	node / gitConfigEmail('ramonlopezpracticas2@gmail.com')}
-  	}
-  	parameters{
-  		stringParam('nombre', defaultValue = 'Julian', description = 'Parametro de cadena para el Job')
-  		choiceParam('planeta', ['Mercurio', 'Venus', 'Tierra', 'Marte', 'Jupiter', 'Saturno', 'Urano', 'Neptuno'])
+    description('Job DSL de ejemplo para el curso de Jenkins')
+    scm {
+        git('https://github.com/macloujulian/jenkins.job.parametrizado.git', 'main') { node ->
+      	    node / gitConfigName('Ramonst18')
+      	    node / gitConfigEmail('Ramonst18@hotmail.com')
+	}
+    }
+    parameters{
+        stringParam('nombre', defaultValue = 'Julian', description = 'Parametro de cadena para el Job')
+  	choiceParam('planeta', ['Mercurio', 'Venus', 'Tierra', 'Marte', 'Jupiter', 'Saturno', 'Urano', 'Neptuno'])
     	booleanParam('agente', false)
-  	}
-  	triggers{
-  		cron('H/7 * * * *')
-  	}
-  	steps {
-  		shell("jobscript.sh")
-  	}
-  	publishers{
-  		mailer("ramonlopezpracticas2@gmail.com", true, true)
-          slackNotifier{
-          notifyAborted(true)
-          notifyEveryFailure(true)
-          notifyNotBuilt(false)
-          notifyUnstable(false)
-          notifyBackToNormal(true)
-          notifySuccess(false)
-          notifyRepeatedFailure(false)
-          startNotification(false)
-          includeTestSummary(false)
-          includeCustomMessage(false)
-          customMessage(null)
-          sendAs(null)
-          commitInfoChoice('NONE')
-          teamDomain(null)
-          authToken(null)
-    	}
-  	}
+    }
+    triggers{
+        cron('H/7 * * * *')
+    }
+    steps {
+        shell("jobscript.sh")
+    }
+    publishers{
+        mailer("ramonlopezpracticas2@gmail.com", true, true)
+        slackNotifier{
+            notifyAborted(true)
+            notifyEveryFailure(true)
+            notifyNotBuilt(false)
+            notifyUnstable(false)
+            notifyBackToNormal(true)
+            notifySuccess(false)
+            notifyRepeatedFailure(false)
+            startNotification(false)
+            includeTestSummary(false)
+            includeCustomMessage(false)
+            customMessage(null)
+            sendAs(null)
+            commitInfoChoice('NONE')
+            teamDomain(null)
+            authToken(null)
+	}
+    }
 }
